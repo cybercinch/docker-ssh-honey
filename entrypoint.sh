5dfbb44b645bf3e33a98401acf72a9854ca98d61
Aaron Guise <guisea@users.noreply.github.com>
2020-07-09 15:21:03 +1200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/ash
+
+# Set Timezone if defined
+if [[ -z "${TZ}" ]]; then
+  ln -sf ${TZ} /etc/localtime
+fi
 ssh-honeypot -r /ssh-honeypot/ssh-honeypot.rsa -p 22 -u nobody
 echo "SSH Honeypot is Running..."
 exec "$@"
